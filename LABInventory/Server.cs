36ad9490d05db82918d76c5beb5bde207fef31a6
Vignesh Akoni Bojan <viakonib@microsoft.com>
2020-01-23 15:14:14 -0800

    Initial Commit

---
+++ LABInventory/Server.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection.Metadata;
+using System.Text;
+
+namespace LABInventory
+{
+    class Server
+    {
+        public string ServerName { get; set; }
+
+        public string SerialNumber { get; set; }
+
+        public int ProductID { get; set; }
+
+        public string ServerOS { get; set; }
+        
+        public decimal IPAddress { get; set; }
+
+        public decimal OSVersion { get; set; }
+
+
+    }
+}
